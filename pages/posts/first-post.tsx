a23b014709d712a99ac36c1f495c3c5346583074
bright0220 <jackfortin0220@gamill.com>
2022-11-29 03:38:57 -0500

    CSS complited

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -5,7 +5,7 @@ import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <Layout>
+        <Layout home>
             <Head>
                 <title>First Post</title>
             </Head>
